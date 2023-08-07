9159dc0f1a6605c1ca26d6a7f52ec3f5d6de52c3
saugusth <54689875+saugusth@users.noreply.github.com>
2023-08-07 00:14:25 -0500

    opperators

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,22 @@
+let x = 10; 
+let y = 5;
+
+let sum = x+y;
+
+console.log(sum);
+
+let diff = x - y;
+
+let product = x * y;
+
+let quotient = x/y;
+
+let remainder = x % y;
+
+let result = x**y;
+
+console.log(diff);
+console.log( product);
+console.log( quotient);
+console.log( remainder);
+console.log( result);
