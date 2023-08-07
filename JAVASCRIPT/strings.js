a07762601a987a8f2ae33a45050d6142ab975319
saugusth <54689875+saugusth@users.noreply.github.com>
2023-08-07 02:11:48 -0500

    strings

---
+++ JAVASCRIPT/strings.js
@@ -0,0 +1,11 @@
+let str1 = "Hello World";
+
+let str2 = "This is a string";
+
+let str3 = '123';
+
+// console.log(typeof str1, typeof str2, typeof str3);
+
+// console.log(`1 + 2 = ${1+2}`);
+
+console.log(str2.length);
