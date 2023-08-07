b41fe37f5f826cbb27fd6be0bfb9475f83f19eae
saugusth <54689875+saugusth@users.noreply.github.com>
2023-08-07 03:17:07 -0500

    string manipulation

--- JAVASCRIPT/strings.js
+++ JAVASCRIPT/strings.js
@@ -1,11 +1,20 @@
-let str1 = "Hello World";
+let str1 = "Hello";
 
-let str2 = "This is a string";
+let str2 = "World";
 
-let str3 = '123';
+// let str3 = '123';
 
 // console.log(typeof str1, typeof str2, typeof str3);
 
 // console.log(`1 + 2 = ${1+2}`);
 
-console.log(str2.length);
+//console.log(str2.length);
+
+// let result = str1 + " " + str2;
+
+// let resultTwo = str1.concat(" " + str2);
+
+// console.log(result);
+// console.log(resultTwo);
+
+let str = "Hello, World";
