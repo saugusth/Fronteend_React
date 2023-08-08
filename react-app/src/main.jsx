73678d7fe64b9aa758f0c7ee61ea400d22b00cc5
saugusth <54689875+saugusth@users.noreply.github.com>
2023-08-08 16:14:06 -0500

    JXS

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const greeting = <h1>Hello, World</h1>
+
+//const greeting = React.createElement('h1', null, "Hello, World");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    {greeting}
   </React.StrictMode>,
 )
