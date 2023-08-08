7c2b2eb862cacf71334bf85658a9f49d9056bb56
saugusth <54689875+saugusth@users.noreply.github.com>
2023-08-08 16:36:00 -0500

    react components

---
+++ react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render() {
+      return <footer>2023 My Website footer</footer>
+    }
+  }
+
+export default Footer
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const greeting = <h1>Hello, World</h1>
-
 //const greeting = React.createElement('h1', null, "Hello, World");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/*<App />*/}
-    {greeting}
+    {<App />}
   </React.StrictMode>,
 )
